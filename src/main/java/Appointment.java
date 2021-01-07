6a57a052b4bff9d49e0d946563d88f11ded4753f
Fatima Gul Kus <47695414+FatimaGK@users.noreply.github.com>
2021-01-07 17:42:36 +0100

    ... (-_-)'

---
+++ src/main/java/Appointment.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.sql.Time;
+
+public class Appointment {
+
+    Time StartTime;
+    Time endTime;
+    String location;
+    String jobDescription;
+
+
+    public Appointment(Time startTime, Time endTime, String location, String jobDescription) {
+        StartTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.jobDescription = jobDescription;
+    }
+
+    public Time getStartTime() {
+        return StartTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setStartTime(Time startTime) {
+        StartTime = startTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public void notification(){}
+    public List<Appointment> viewCurrentAppointment(Plumber idPlumber){
+        return appointmnet;
+    }
+
+}
