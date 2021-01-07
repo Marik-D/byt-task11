9217d4ea9150c7d80c252ccf61395c9e0af5655e
PaulieGualtieri <PiotrBosak@yahoo.com>
2021-01-07 16:20:35 +0100

    added plumber class

---
+++ src/Plumber.java
@@ -0,0 +1,11 @@
+public class Plumber {
+    private String qualifications;
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
+}
